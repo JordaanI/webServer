b3518f520e3df37ecd214bc2dfe08e7f65e270f4
Ivan <jordaanivan@gmail.com>
2024-03-25 09:30:08 -0400

    Beta release 1.0.0, Refacor defines, exception handling

--- server.scm
+++ server.scm
@@ -27,7 +27,7 @@
 ;;;; Server Name and version
 ;;;
 
-(define server-version "alpha 0.0.1")
+(define server-version "beta 0.0.0")
 
 (define server-name "Iron_Pig_WebServer")
 
@@ -56,124 +56,191 @@
 ;;;
 
 (define (serve connection)
-					; Read Headers
-  (define (read-http-headers)
-    (let loop ((line (read-line connection)))
-      (if (string=? line "") '()
-	  (append (split-string line #\space) (loop (read-line connection))))))
-					; Activate Function
-  (define (activate-fnq fnq)
-    (eval (string->symbol fnq)))
-					; Parse Path
-  (define (parse-path path)
-					; Parse Args
-    (define (parse-args ap)
-      (cons (string->keyword (car ap)) (cdr ap)))
-    
-    (let* ((nsf-pair (cdr (split-string path #\/)))
-	   (ns (string-append (car nsf-pair) "#"))
-	   (f-a (split-string (cadr nsf-pair) #\?))
-	   (a (if (> (length f-a) 1) (split-string (cadr f-a) #\&) '()))
-	   (args (flatten
-		  (map parse-args
-		       (map (lambda (a)
-			      (split-string a #\=))
-			    a)))))
-      (list->table `((fnq . ,(string-append ns (car f-a))) (args ,@args)))))
-					; Answer 200/1
-  
-  (define (answer-OK type return #!key (code "200 OK\n"))
-    (display (string-append
-	      protocol code
-	      "Content-Type: " type  "\n"
-	      "Content-Length: " (number->string (string-length return))
-	      "\r\n\r\n"
-	      return)
-	     connection))
-    					; Answer 204
-  (define (answer-204)
-    (display (string-append
-	      protocol "204 No Content\n"
-	      "Server: " server-name-and-version)
-	     connection))
-					; Answer 400
-  (define (answer-400 reason)
-    (let ((reason (table-ref t400 reason)))
-      (display (string-append
-		protocol  "400 Bad Request\n"
-		"Content-Type: text/plain\n"
-		"Content-Length: " (number->string (+ (string-length reason) 13))
-		"\r\n\r\n"
-		"Bad Request: " reason)
-	       connection)))
-					; Answer 404
-  (define (answer-404)
-    (display (string-append
-	      protocol  "404 Not Found\n"
-	      "Content-Type: text/plain\n"
-	      "Content-Length: 13\r\n\r\n"
-	      "Not Found")
-	     connection))
-					; Answer 411
-  (define (answer-411)
-    (display (string-append
-	      protocol "411 Length Required"
-	      "Server: " server-name-and-version)))
-					; Serve GET
-  (define (serve-get parsed-path server-info)
-    (log "GET" parsed-path specifics: server-info)
-    (let ((return (apply (activate-fnq (table-ref parsed-path 'fnq)) (table-ref parsed-path 'args))))
-      (cond
-       ((table? return)
-	(answer-OK "application/json" (table->json-string return)))
-       ((and (string? return) (zero? (string-length return)))
-	(answer-204))
-       ((or (string? return) (number? return))
-	(let ((return-string (if (number? return) (number->string return) return)))
-	  (answer-OK "text/plain" return-string)))
-       (#t (answer-404)))))
-					; Serve POST
-  (define (serve-post parsed-path server-info)
-
-    (define (read-body content-length)
-      (if (zero? content-length) '()
-	  (cons (read-char connection) (read-body (- content-length 1)))))
-
-    (define (read-string-number body)
-      (let ((number? (string->number body)))
-	(if number? number? body)))
-    
-    (let* ((content-type (cdr (assoc "Content-Type:" server-info)))
-	   (content-length (string->number (cdr (assoc "Content-Length:" server-info))))
-	   (body (list->string (read-body content-length)))
-	   (args (cond
-		  ((equal? content-type "application/json") (list (string->keyword "json-data") (json-string->table body)))
-		  ((equal? content-type "text/plain") (list (string->keyword "plain") (read-string-number body)))
-		  (#t #f))))
-     
-      (if args
-	  (begin
-	    (log "POST" parsed-path specifics: server-info)
-	    (let ((return (apply (activate-fnq (table-ref parsed-path 'fnq)) args)))
-	      (cond
-	       ((table? return)
-		(answer-OK "application/json" (table->json-string return) code: "201 Created"))
-	       ((string? return)
-		(answer-OK "text/plain" return code: "201 Created"))
-	       ((number? return)
-		(answer-OK "text/plain" (number->string return)) code: "201 Created")
-	       ((equal? #!void return)
-		(answer-OK "text/plain" "" code: "201 Created")))))
-	  (answer-400 'unknown-content-type))))
-					; Serve Connection Body
-  (let* ((headers (read-http-headers))
+  (let* ((headers (read-http-headers connection))
 	 (request (car headers))
 	 (parsed-path (parse-path (cadr headers))))
     (cond
-     ((equal? request "GET") (serve-get parsed-path (pairize (list-tail headers 3))))
-     ((equal? request "POST") (serve-post parsed-path (pairize (list-tail headers 3))))
-     (#t (answer-404)))
+     ((equal? request "GET") (serve-get connection parsed-path (pairize (list-tail headers 3))))
+     ((equal? request "POST") (serve-post connection parsed-path (pairize (list-tail headers 3))))
+     (#t (answer-404 connection)))
     (close-port connection)))
+
+;;;
+;;;; Read http headers
+;;;
+
+(define (read-http-headers connection)
+  (let loop ((line (read-line connection)))
+    (if (string=? line "") '()
+	(append (split-string line #\space) (loop (read-line connection))))))
+;;;
+;;;; Activate Function
+;;;
+
+(define (activate-fnq fnq)
+  (eval (string->symbol fnq)))
+
+;;;
+;;;; Parse Path
+;;;
+
+(define (parse-path path)    
+  (let* ((nsf-pair (cdr (split-string path #\/)))
+	 (ns (string-append (car nsf-pair) "#"))
+	 (f-a (split-string (cadr nsf-pair) #\?))
+	 (a (if (> (length f-a) 1) (split-string (cadr f-a) #\&) '()))
+	 (args (flatten
+		(map parse-args
+		     (map (lambda (a)
+			    (split-string a #\=))
+			  a)))))
+    (list->table `((fnq . ,(string-append ns (car f-a))) (args ,@args)))))
+
+;;;
+;;;; Parse Args
+;;;
+
+(define (parse-args ap)
+  (cons (string->keyword (car ap)) (cdr ap)))
+
+;;;
+;;;;Answer-OK
+;;;
+
+(define (answer-OK connection type return #!key (code "200 OK\n"))
+  (display (string-append
+	    protocol code
+	    "Content-Type: " type  "\n"
+	    "Content-Length: " (number->string (string-length return))
+	    "\r\n\r\n"
+	    return)
+	   connection))
+
+;;;
+;;;;Answer 204
+;;;
+
+(define (answer-204 connection)
+  (display (string-append
+	    protocol "204 No Content\n"
+	    "Server: " server-name-and-version)
+	   connection))
+
+;;;
+;;;; Answer 400
+;;;
+
+(define t-400 (make-table init: "Generic Error"))
+(table-set! t-400 'unknown-content-type "Unsupported Content Type . . .")
+
+(define (answer-400 connection reason)
+  (let ((reason (table-ref t-400 reason)))
+    (display (string-append
+	      protocol  "400 Bad Request\n"
+	      "Content-Type: text/plain\n"
+	      "Content-Length: " (number->string (+ (string-length reason) 13))
+	      "\r\n\r\n"
+	      "Bad Request: " reason)
+	     connection)))
+
+;;;
+;;;; Answer 404
+;;;
+
+(define (answer-404 connection)
+  (display (string-append
+	    protocol  "404 Not Found\n"
+	    "Content-Type: text/plain\n"
+	    "Content-Length: 9\r\n\r\n"
+	    "Not Found")
+	   connection))
+
+;;;
+;;;; Answer 411
+;;;
+
+(define (answer-411 connection)
+  (display (string-append
+	    protocol "411 Length Required\n"
+	    "Server: " server-name-and-version
+	    "\nContent-Type: plain/text\n"
+	    "Content-Length: 23\r\n\r\n"
+	    "Content Length Required")
+	   connection))
+
+;;;
+;;;; Return Checker
+;;;
+
+(define (check-return connection fnq args)
+  (with-exception-catcher
+   (lambda (exc)
+     'no-function-exists)
+   (lambda ()
+     (apply (activate-fnq fnq) args))))
+
+;;;
+;;;; Serve GET
+;;;
+
+(define (serve-get connection parsed-path server-info)
+  (log "GET" parsed-path specifics: server-info)
+  (let ((return (check-return connection (table-ref parsed-path 'fnq) (table-ref parsed-path 'args))))
+    (cond
+     ((table? return)
+      (answer-OK connection "application/json" (table->json-string return)))
+     ((and (string? return) (zero? (string-length return)))
+      (answer-204 connection))
+     ((or (string? return) (number? return))
+      (let ((return-string (if (number? return) (number->string return) return)))
+	(answer-OK connection "text/plain" return-string)))
+     (#t (answer-404 connection)))))
+
+;;;
+;;;; Read Body
+;;;
+
+(define (read-body connection content-length)
+  (if (zero? content-length) '()
+      (cons (read-char connection) (read-body connection (- content-length 1)))))
+
+;;;
+;;;; Read String Number
+;;;
+
+(define (read-string-number body)
+  (let ((number? (string->number body)))
+    (if number? number? body)))
+
+;;;
+;;;; Serve POST
+;;;
+
+(define (serve-post connection parsed-path server-info)
+  (log "POST" parsed-path specifics: server-info)
+  (let ((content-length (assoc "Content-Length:" server-info)))
+    (if content-length
+	(let* ((content-type (cdr (assoc "Content-Type:" server-info)))
+	       (content-length (string->number (cdr content-length)))
+	       (body (list->string (read-body connection content-length)))
+	       (args (cond
+		      ((equal? content-type "application/json") (list (string->keyword "json-data") (json-string->table body)))
+		      ((equal? content-type "text/plain") (list (string->keyword "plain") (read-string-number body)))
+		      (#t #f))))
+	  (if args
+	      (let ((return (check-return connection (table-ref parsed-path 'fnq) args)))
+		(cond
+		 ((table? return)
+		  (answer-OK connection  "application/json" (table->json-string return) code: "201 Created"))
+		 ((string? return)
+		  (answer-OK connection "text/plain" return code: "201 Created"))
+		 ((number? return)
+		  (answer-OK connection "text/plain" (number->string return)) code: "201 Created")
+		 ((equal? #!void return)
+		  (answer-OK conneciton "text/plain" "" code: "201 Created"))
+		 ((equal? return 'no-function-exists) (answer-404 connection))))
+	      (answer-400 connection 'unknown-content-type)))
+	(answer-411 connection))))
 					; Logging Framework
 (define (log command parsed-path #!key (specifics '()))
   (with-output-to-file (list path: ".server_log" append: #t)
@@ -185,12 +252,6 @@
 						  (string-append (car pair) " " (cdr pair) "\n")) specifics))
 		      "<=====================================================================================>"
 		      "\r\n\r\n")))))
-					; Bad Request Table
-(define t-400 (make-table init: "Generic Error"))
-(table-set! t-400 'unknown-content-type "Unsupported Content Type . . .")
 					; Debugging statements
-
 (define (debug #!key plain)
   "SERVED")
-
-(server)
